b4644ebabd6ebc22f8b43973566e33628c5445c8
tecon321 <60271281+tecon321@users.noreply.github.com>
2020-11-16 19:44:19 -0700

    Update blackJack2.js

--- blackJack2.js
+++ blackJack2.js
@@ -333,7 +333,7 @@ function endGame()
     //write new values to database (display?)
 
     //switch to game room screen
-    //window.location.href = 'https://w3docs.com';
+    window.location.href = './blackjackHome.html';
 
 
 }
